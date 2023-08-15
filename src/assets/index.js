cda0d8af0f334d33214f0f6a94a390149be7474a
Ayiks <cliffysarp@gmail.com>
2023-08-15 13:32:35 +0000

    images additions

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import close from './icons/close.png';
 
 /* images*/
 import hero1 from './hero/hero1.jpg';
+import hero2 from './hero/hero2.jpg'
 
 
 
@@ -12,4 +13,5 @@ export {
     menu,
     close,
     hero1,
+    hero2,
 }
