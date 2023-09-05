d969147a5526618263aa15b3d499690a9949bb86
Ayiks <cliffysarp@gmail.com>
2023-09-05 12:23:25 +0000

    addition of whyUs and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
-import {Navbar, Hero, HeroFooter} from "./components";
+import {Navbar, Hero, HeroFooter,WhyUs, Footer} from "./components";
 const App =()  =>{
 
   return (
   <BrowserRouter>
-  <div className="relative z-10">
+  <div className="relative z-0">
     <div>
       <Navbar />
       <Hero />
     </div>
+
    <div>
     <HeroFooter />
+   </div>
+
+   <div className="bg-about bg-cover bg-center bg-no-repeat pb-10 flex items-center text-center">
+    <WhyUs />
+   </div>
+
+   {/* footer */}
+   <div className="p-10 bg-footer -ml-10">
+    <Footer />
    </div>
   </div>
   </BrowserRouter>
