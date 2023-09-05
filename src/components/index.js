b67fcfb2bef1798d37d11a642b056d98d8e6d3d5
Ayiks <cliffysarp@gmail.com>
2023-09-05 12:25:06 +0000

    addition of new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import HeroFooter from "./HeroFooter";
+import WhyUs from "./WhyUs";
+import Footer from "./Footer";
 
-export{Navbar, Hero, HeroFooter};
+export{Navbar, Hero, HeroFooter, WhyUs, Footer};
