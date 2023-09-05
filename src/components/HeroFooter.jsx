f925fb9c08006ea115f9340c490340c463cf03dd
Ayiks <cliffysarp@gmail.com>
2023-09-05 12:24:51 +0000

    hero footer

--- src/components/HeroFooter.jsx
+++ src/components/HeroFooter.jsx
@@ -11,18 +11,22 @@ const HeroFooter = () =>{
     <div className="-mt-[6rem]">
         <motion.div
         variants={textVariant()}>
-            <div className="bg-orangePeel flex items-center text-center  content-center p-10 mt-6 sm:mt-20">
-           <div>
+            <div className="bg-orangePeel flex items-center text-center content-between p-10 mt-6 sm:mt-20">
+           <div className="text-left">
            <h3
                  className={`uppercase text-[12px] sm:text-[30px] text-night font-bold`}
                 >Already have an excellent idea?</h3>
                  <h3
-                 className={`uppercase text-[12px] sm:text-[30px] text-night font-bold`}
-                >Already have an excellent idea?</h3>
+                 className={`uppercase text-[12px] sm:text-[20px] text-night font-bold`}
+                >And want to know whether it's possible to implement?</h3>
            </div>
-           <div className="h-full w-[200px] bg-black">
+           {/* <div className="h-full w-[400px] bg-black text-right">
             <p>sfhsjfhj</p>
-           </div>
+            <p>sfhsjfhj</p>
+            <p>sfhsjfhj</p>
+            <p>sfhsjfhj</p>
+            <p>sfhsjfhj</p>
+           </div> */}
             </div>
         </motion.div>
     </div>
