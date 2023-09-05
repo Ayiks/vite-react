d6e9106e41819c15166dec4a54231fd3bf1595e2
Ayiks <cliffysarp@gmail.com>
2023-09-05 12:23:40 +0000

    import of new images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,17 @@
 import menu from './icons/menu.png';
 import close from './icons/close.png';
 
+/* logos*/
+import eth from './logos/logo.png';
+
 /* images*/
 import hero1 from './hero/hero1.jpg';
-import hero2 from './hero/hero2.jpg'
+import hero2 from './hero/hero2.jpg';
+
+import innovation from './images/innovation.jpg';
+
+/* footer image */
+import footerImage from './footer/footer.jpg';
 
 
 
@@ -14,4 +22,7 @@ export {
     close,
     hero1,
     hero2,
+    innovation,
+    eth,
+    footerImage,
 }
